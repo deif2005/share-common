8b8b05afade2de3ffc705bd5de91bdef36dcaa96
defi <deif2005@users.noreply.github.com>
2018-07-16 11:13:23 +0800

    验证码功能

--- src/main/java/com/usi/util/VertifyCodeUtil.java
+++ src/main/java/com/usi/util/VertifyCodeUtil.java
@@ -182,6 +182,17 @@ public class VertifyCodeUtil {
         return sBuffer.toString();
     }
 
+    /**
+     * 获取四位纯数字随机数
+     * @return
+     */
+    public static String getRandromNum(){
+        int x;//定义两变量
+        Random ne = new Random();//实例化一个random的对象ne
+        x = ne.nextInt(9999-1000+1)+1000;//为变量赋随机值1000-9999
+        return String.valueOf(x);
+    }
+
     public int getWidth() {
         return width;
     }
